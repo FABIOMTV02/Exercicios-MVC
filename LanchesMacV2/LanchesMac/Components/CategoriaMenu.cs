ed58fce9826879be8acadea3c3c0d7db5eda316e
Fabio <fabiomtv@outlook.com.br>
2023-03-19 14:28:16 -0300

    viewcomponent

---
+++ LanchesMacV2/LanchesMac/Components/CategoriaMenu.cs
@@ -0,0 +1,22 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using NuGet.Protocol.Core.Types;
+
+namespace LanchesMac.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriarRepository;
+        public CategoriaMenu(ICategoriaRepository categoriarRepository)
+        {
+            _categoriarRepository = categoriarRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriarRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
