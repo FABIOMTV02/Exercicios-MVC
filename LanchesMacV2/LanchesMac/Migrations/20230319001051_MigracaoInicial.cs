fbecc4f4e092f080d5d6c9ae231940be3db8dc60
Fabio <fabiomtv@outlook.com.br>
2023-03-18 21:45:43 -0300

    tranferencia pro note

--- LanchesMacV2/LanchesMac/Migrations/20230319001051_MigracaoInicial.cs
+++ LanchesMacV2/LanchesMac/Migrations/20230319001051_MigracaoInicial.cs
@@ -51,6 +51,31 @@ namespace LanchesMac.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "CarrinhoCompraItens",
+                columns: table => new
+                {
+                    CarrinhoCompraItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LancheId = table.Column<int>(type: "int", nullable: true),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    CarrinhoCompraId = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoCompraItens", x => x.CarrinhoCompraItemId);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoCompraItens_Lanches_LancheId",
+                        column: x => x.LancheId,
+                        principalTable: "Lanches",
+                        principalColumn: "LancheId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoCompraItens_LancheId",
+                table: "CarrinhoCompraItens",
+                column: "LancheId");
+
             migrationBuilder.CreateIndex(
                 name: "IX_Lanches_CategoriaId",
                 table: "Lanches",
@@ -60,6 +85,9 @@ namespace LanchesMac.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "CarrinhoCompraItens");
+
             migrationBuilder.DropTable(
                 name: "Lanches");
 
--- LanchesMacV2/LanchesMac/Migrations/20230319001827_PopularLanches.cs
+++ LanchesMacV2/LanchesMac/Migrations/20230319001827_PopularLanches.cs
@@ -4,10 +4,8 @@
 
 namespace LanchesMac.Migrations
 {
-    /// <inheritdoc />
     public partial class PopularLanches : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1,'Pão, hambúrger, ovo, presunto, queijo e batata palha','Delicioso pão de hambúrger com ovo frito; presunto e queijo de primeira qualidade acompanhado com batata palha',1, 'http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg','http://www.macoratti.net/Imagens/lanches/cheesesalada1.jpg', 0 ,'Cheese Salada', 12.50)");
@@ -15,9 +13,7 @@ namespace LanchesMac.Migrations
             migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(1,'Pão, hambúrger, presunto, mussarela e batalha palha','Pão de hambúrger especial com hambúrger de nossa preparação e presunto e mussarela; acompanha batata palha.',1,'http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg',0,'Cheese Burger', 11.00)");
             migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco) VALUES(2,'Pão Integral, queijo branco, peito de peru, cenoura, alface, iogurte','Pão integral natural com queijo branco, peito de peru e cenoura ralada com alface picado e iorgurte natural.',1,'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg','http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg',1,'Lanche Natural Peito Peru', 15.00)");
         }
- 
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql("DELETE FROM Lanches");
