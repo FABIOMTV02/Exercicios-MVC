a2f8769b05fa7b0d2eac97858fbbfd0d5df68bf4
Fábio Martins <fabiomtv@outlook.com.br>
2023-04-06 15:04:10 -0300

    iniciando metodo Login

---
+++ LanchesMacV2/LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace LanchesMac.ViewModels
+{
+    public class LoginViewModel
+    {
+
+        //Biblioteca do Requiredd diferente do professor possivel erro, aula 75
+        [Required(ErrorMessage = "Informe o nome")]
+        [Display(Name = "Usuario")]
+
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl{ get; set; }
+    }
+}
