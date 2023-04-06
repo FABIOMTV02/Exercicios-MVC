807a00565156b0a0d7e61f77f628530aa67ced3d
Fábio Martins <fabiomtv@outlook.com.br>
2023-04-06 13:09:54 -0300

    finalizando pedidocontroller

--- LanchesMacV2/LanchesMac/Controllers/PedidoController.cs
+++ LanchesMacV2/LanchesMac/Controllers/PedidoController.cs
@@ -1,6 +1,7 @@
 ﻿using LanchesMac.Models;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 
 namespace LanchesMac.Controllers
 {
@@ -24,7 +25,48 @@ namespace LanchesMac.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //obtem os itens do carrinho de compra do cliente
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //Verifica se existem itens de pedido
+            if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche....");
+            }
+
+            //Calcula o total de itens do pedido
+            foreach(var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //Atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //valida os dados do pedido
+            if(ModelState.IsValid)
+            {
+                //cria o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //Limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //Exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+
+            }
+            return View(pedido);
         }
     }
 }
