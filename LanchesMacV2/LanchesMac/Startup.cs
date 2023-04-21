dd11fa45f7ae3f538dbe83e04d4a893f90bef37b
Fábio Martins <fabiomtv@outlook.com.br>
2023-04-20 21:58:42 -0300

    finalizando seed roles

--- LanchesMacV2/LanchesMac/Startup.cs
+++ LanchesMacV2/LanchesMac/Startup.cs
@@ -2,6 +2,7 @@
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +28,8 @@ public class Startup
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<IPedidoRepository,PedidoRepository>();
+        services.AddScoped<ISeedUserRoleInitial,SeedUserRoleInitial>();
+
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 
@@ -37,7 +40,8 @@ public class Startup
 
     }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, 
+        ISeedUserRoleInitial seedUserRoleInitial)
     {
         if (env.IsDevelopment())
         {
@@ -52,6 +56,11 @@ public class Startup
 
         app.UseStaticFiles();
         app.UseRouting();
+
+        //cria os perfis
+        seedUserRoleInitial.SeedRoles();
+        //cria os usuarios e atribui ao perfil
+        seedUserRoleInitial.SeedUsers();
 
         app.UseSession();
 
